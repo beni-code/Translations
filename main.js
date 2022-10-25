28ebcec117eff73155312839dfc347d2cfd531d8
ben.li <53158397+beni-code@users.noreply.github.com>
2022-10-25 13:42:41 +0800

    1

---
+++ main.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+var md = require('markdown-it')();
+
+let str = fs.readFileSync("./test.md",'utf-8')
+
+var result = md.render(str);
+
+while (result.indexOf("&lt;$") >= 0) {
+    let startIndex = result.indexOf("&lt;$")
+    let endIndex = result.indexOf("$&gt;",17)+4
+
+    let midStr = result.substring(startIndex,endIndex+1)
+    let preStr = result.substring(0,startIndex)
+    let sufStr = result.substring(endIndex+1,result.length)
+
+    let midendindex = midStr.indexOf("$}&amp;&amp;")
+    let yuanwenstr= midStr.slice(7,midendindex)
+    let yiwenstr = midStr.slice(midendindex+14,midStr.length - 7)
+
+    let span = `<span class="translateSpan" title="${yiwenstr}">${yuanwenstr}</span>`
+
+    result = preStr+span+sufStr;
+}
+
+fs.writeFileSync("./data.js",`let md = \`${result}\``,'utf-8')
+
+
